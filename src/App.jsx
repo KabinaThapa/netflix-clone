9185d7c031846ec68fab662ef0320346e2175a8b
Kabina Thapa <thapakabina28@gmail.com>
2023-05-28 17:17:18 -0400

    react-vite and bootstrap environment setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="container">
+        <header className="header">
+          <div className='dropdown'>
+          <button>English </button>
+          </div>
+        </header>
+      </div>
+    </>
+  )
+}
+
+export default App
